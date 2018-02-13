c9884f00f59327d1ced8fa73fa9cfdb01483f904
hindrekhan <hindrek.hannus@tptlive.ee>
2018-02-13 09:33:15 +0200

    a

---
+++ OnPlug.ps1
@@ -0,0 +1,9 @@
+﻿$SoundFileUrl = "https://superuser.com/questions/82053/setting-home-in-powershell"
+$SoundFilePath = ".\Desktop\yo.html"
+
+function Run() {
+    Invoke-WebRequest $SoundFileUrl -OutFile $SoundFilePath
+    Start-Process -FilePath sndvol.exe 
+}
+
+Run
